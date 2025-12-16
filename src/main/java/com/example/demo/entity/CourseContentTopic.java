d138c1ccd28f137186934f205b6c52c5317f6dc6
bala-amypo <bala@amypo.in>
2025-12-16 10:35:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity;
 
 public class CourseContentTopic {
-    private 
+    private Long id;
+    private Course course;
+    private String topicName;
+    private Double weightPercentage;
 }
