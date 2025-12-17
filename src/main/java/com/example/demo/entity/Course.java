e61ff626cc1d7563b1b71debf9125647e89cd6e2
bala-amypo <bala@amypo.in>
2025-12-17 09:56:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -1,7 +1,9 @@
 package com.example.demo.entity;
 
 public class Course {
+    @Id @GeneratedValue
     private Long id;
+    @ManyToOne
     private University university;
     private String courseCode;
     private String courseName;
