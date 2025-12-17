efe4fcd462062a0336afa3246c83ca61fa8fe33c
bala-amypo <bala@amypo.in>
2025-12-17 10:11:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 public class University {
     @Id @GeneratedValue
     private Long id;
+    @column
     private String name;
     private String accreditationLevel;
     private String country;
