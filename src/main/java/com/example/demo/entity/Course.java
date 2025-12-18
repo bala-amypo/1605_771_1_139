320c3ee96f1174562428f900bb5540a8b22b5596
bala-amypo <bala@amypo.in>
2025-12-18 06:29:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -2,12 +2,5 @@ package com.example.demo.entity;
 
 
 public class Course {
-    private Long id;
-    private University university;
-    private String courseCode;
-    private String courseName;
-    private Integer creditHours;
-    private String description;
-    private String department;
-    private Boolean active;
+   
 }
--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -1,8 +1,5 @@
 package com.example.demo.entity;
 
 public class CourseContentTopic {
-    private Long id;
-    private Course course;
-    private String topicName;
-    private Double weightPercentage;
+
 }
