2937cddacd28e9d5c8994e023582f0618090a554
bala-amypo <bala@amypo.in>
2025-12-19 09:25:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,5 +1,52 @@
 package com.example.demo.controller;
 
-public class CourseController{
-    
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.Course;
+import com.example.demo.service.CourseService;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService service;
+
+    public CourseController(CourseService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return new ResponseEntity<>(service.create(course), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @GetMapping("/code/{code}")
+    public ResponseEntity<Course> getByCode(@PathVariable String code) {
+        return ResponseEntity.ok(service.getByCode(code));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(
+            @PathVariable Long id,
+            @RequestBody Course course) {
+        return ResponseEntity.ok(service.update(id, course));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
