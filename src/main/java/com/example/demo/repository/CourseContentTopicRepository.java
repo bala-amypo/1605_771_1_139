fb04f2d9330b4425e438fd3842b411ac289db3c5
bala-amypo <bala@amypo.in>
2025-12-19 05:31:07 +0000

    User commit

--- src/main/java/com/example/demo/repository/CourseContentTopicRepository.java
+++ src/main/java/com/example/demo/repository/CourseContentTopicRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.TransferEvaluationResult;
+import com.example.demo.entity.CourseContentTopic;
 
-public interface TransferEvaluationResultRepository extends JpaRepository<TransferEvaluationResult, Long> {
-    List<TransferEvaluationResult> findByStudentId(String studentId);
+public interface CourseContentTopicRepository extends JpaRepository<CourseContentTopic, Long> {
+    Optional<CourseContentTopic> findByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCase(String name);
 }
