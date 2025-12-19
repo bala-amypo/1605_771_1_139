ed08f2ea9ffe81891da3b29aa56c42b3f7557dbe
bala-amypo <bala@amypo.in>
2025-12-19 08:58:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -1,6 +1,51 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "universities", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class University {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String country;
+
+    public University() {
+    }
+
+    public University(Long id, String name, String country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
