105f17ac4d8423c0c8f0803d3ba965d20ac767d4
bala-amypo <bala@amypo.in>
2025-12-24 08:13:31 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,24 +1,31 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class AuthRequest {
+    
+    @Email
+    @NotBlank
+    private String email;
 
+    @NotBlank
+    private String password;
 
-private String email;
-private String password;
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-public AuthRequest() {}
+    public String getPassword() {
+        return password;
+    }
 
-
-public AuthRequest(String email, String password) {
-this.email = email;
-this.password = password;
-}
-
-
-public String getEmail() { return email; }
-public void setEmail(String email) { this.email = email; }
-public String getPassword() { return password; }
-public void setPassword(String password) { this.password = password; }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+     
 }
