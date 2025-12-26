84afc8f6f1e2ab76c64207c5b380074ad726a9f1
bala-amypo <bala@amypo.in>
2025-12-26 08:33:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/UniversityController.java
+++ src/main/java/com/example/demo/controller/UniversityController.java
@@ -7,11 +7,32 @@ import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.University;
 
 @RestController
-@RequestMapping("/universities")
+@RequestMapping("/api/universities")
+@Tag(name = "University")
 public class UniversityController {
+
+    @PostMapping
+    public University create(@RequestBody University university) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public University update(@PathVariable Long id,
+                             @RequestBody University university) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public University getById(@PathVariable Long id) {
+        return null;
+    }
 
     @GetMapping
     public List<University> getAll() {
-        return List.of();
+        return null;
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public void deactivate(@PathVariable Long id) {
     }
 }
