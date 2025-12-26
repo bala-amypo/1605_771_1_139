42c50303045d3049ec62090c720816d5f1a343f8
bala-amypo <bala@amypo.in>
2025-12-26 08:33:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -7,11 +7,32 @@ import org.springframework.web.bind.annotation.*;
 import com.example.demo.entity.Course;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/courses")
+@Tag(name = "Course")
 public class CourseController {
 
-    @GetMapping
-    public List<Course> getAllCourses() {
-        return List.of();
+    @PostMapping
+    public Course create(@RequestBody Course course) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Course update(@PathVariable Long id,
+                         @RequestBody Course course) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Course getById(@PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping("/university/{universityId}")
+    public List<Course> getByUniversity(@PathVariable Long universityId) {
+        return null;
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public void deactivate(@PathVariable Long id) {
     }
 }
