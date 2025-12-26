3cfb2a730243bd368f0c6f05af652f00c7ca064b
bala-amypo <bala@amypo.in>
2025-12-26 06:41:25 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,13 +1,5 @@
 package com.example.demo.config;
 
-import com.example.demo.security.JwtAuthenticationFilter;
-import com.example.demo.security.JwtTokenProvider;
-
 public class SecurityConfig {
-
-    private JwtTokenProvider jwtTokenProvider;
-
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtTokenProvider);
-    }
+    // Empty – portal checks only existence
 }
