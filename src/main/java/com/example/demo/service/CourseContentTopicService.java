bbe0e52d6f73eacd1190bc818cc9fa0ca8b6fe31
bala-amypo <bala@amypo.in>
2025-12-26 14:35:16 +0000

    User commit

--- src/main/java/com/example/demo/service/CourseContentTopicService.java
+++ src/main/java/com/example/demo/service/CourseContentTopicService.java
@@ -1,12 +1,10 @@
 package com.example.demo.service;
 
-import java.util.List;
 import com.example.demo.entity.CourseContentTopic;
+import java.util.List;
 
 public interface CourseContentTopicService {
-    CourseContentTopic create(CourseContentTopic topic);
-    CourseContentTopic getById(Long id);
-    List<CourseContentTopic> getAll();
-    CourseContentTopic update(Long id, CourseContentTopic topic);
-    void delete(Long id);
+    CourseContentTopic addTopic(Long courseId, CourseContentTopic topic);
+    List<CourseContentTopic> getTopicsByCourseId(Long courseId);
+    void deleteTopic(Long id);
 }
