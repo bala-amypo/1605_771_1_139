7d1d37f31b11f15b4e7c6dce0acba1870364d65e
bala-amypo <bala@amypo.in>
2025-12-26 15:13:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,13 +1,9 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.util.Set;
 
 @Entity
-@Data
-@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
@@ -22,9 +18,23 @@ public class User {
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<String> roles;
 
+    public User() {}
+
     public User(String email, String password, Set<String> roles) {
         this.email = email;
         this.password = password;
         this.roles = roles;
     }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public Set<String> getRoles() { return roles; }
+    public void setRoles(Set<String> roles) { this.roles = roles; }
 }
