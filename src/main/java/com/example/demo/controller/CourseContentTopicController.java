f2983b716a3f11b606c61139623467c669ee7822
bala-amypo <bala@amypo.in>
2025-12-27 09:21:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseContentTopicController.java
+++ src/main/java/com/example/demo/controller/CourseContentTopicController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 import com.example.demo.entity.CourseContentTopic;
-import com.example.demo.service.CourseContentTopicService; // Ensure interface name matches
+import com.example.demo.service.CourseContentTopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
