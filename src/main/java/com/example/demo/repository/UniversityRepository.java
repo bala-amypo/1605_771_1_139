d4509dc77aa977be062947d980a0f68a2b8d0e54
bala-amypo <bala@amypo.in>
2025-12-27 08:26:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/UniversityRepository.java
+++ src/main/java/com/example/demo/repository/UniversityRepository.java
@@ -1,8 +1,5 @@
 package com.example.demo.repository;
 import com.example.demo.entity.University;
-package com.example.demo.repository;
-
-import com.example.demo.model.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
