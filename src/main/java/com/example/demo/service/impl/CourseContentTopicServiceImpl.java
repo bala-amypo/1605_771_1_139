f220f981b21986a44a0ac24511abf04cd2b51fcf
bala-amypo <bala@amypo.in>
2025-12-27 05:13:14 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CourseContentTopicServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CourseContentTopicServiceImpl.java
@@ -9,36 +9,3 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.ArrayList;
 
-@Service
-public class CourseContentTopicServiceImpl implements CourseContentTopicService {
-
-    @Autowired
-    private CourseContentTopicRepository courseContentTopicRepository;
-
-    @Override
-    public CourseContentTopic createTopic(CourseContentTopic t) {
-        return courseContentTopicRepository.save(t);
-    }
-
-    @Override
-    public CourseContentTopic updateTopic(Long id, CourseContentTopic t) {
-        t.setId(id);
-        return courseContentTopicRepository.save(t);
-    }
-
-    @Override
-    public CourseContentTopic getTopicById(Long id) {
-        return courseContentTopicRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public List<CourseContentTopic> getTopicsForCourse(Long courseId) {
-        
-        return new ArrayList<>();
-    }
-
-    @Override
-    public void deleteTopic(Long id) {
-        courseContentTopicRepository.deleteById(id);
-    }
-}
