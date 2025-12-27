a816ec8e103591ed659172e0ed21ad31dfe97905
bala-amypo <bala@amypo.in>
2025-12-27 05:57:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,26 +2,31 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) {
+        // The error was likely here. We return ResponseEntity<User> to match the service return type.
         return ResponseEntity.ok(userService.register(user));
     }
 
-    @PostMapping("/login") // Sometimes login is here, sometimes in AuthController
-    public ResponseEntity<String> login(@RequestBody User user) {
-        return ResponseEntity.ok(userService.login(user.getEmail(), user.getPassword()));
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
