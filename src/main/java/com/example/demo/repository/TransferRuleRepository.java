501e0d87aa4a8f3da8a2766ed8f91b7b76c43df0
bala-amypo <bala@amypo.in>
2025-12-28 12:40:10 +0000

    User commit

--- src/main/java/com/example/demo/repository/TransferRuleRepository.java
+++ src/main/java/com/example/demo/repository/TransferRuleRepository.java
@@ -2,6 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.TransferRule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface TransferRuleRepository extends JpaRepository<TransferRule, Long> {
+
+    Optional<TransferRule> findBySourceUniversityIdAndTargetUniversityId(
+            Long sourceUniversityId,
+            Long targetUniversityId
+    );
 }
