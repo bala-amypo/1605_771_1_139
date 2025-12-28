b3aff166e1ef6d9630a50513bf75788c246dcd87
bala-amypo <bala@amypo.in>
2025-12-28 13:57:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -1,59 +1,14 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Course {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 🔥 PORTAL EXPECTS THIS EXACT NAME
     private String courseName;
-
-    private int creditHours;
-
-    private boolean active;
-
-    @ManyToOne
-    private University university;
-
-    // ===== GETTERS & SETTERS =====
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public int getCreditHours() {
-        return creditHours;
-    }
-
-    public void setCreditHours(int creditHours) {
-        this.creditHours = creditHours;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public University getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(University university) {
-        this.university = university;
-    }
 }
