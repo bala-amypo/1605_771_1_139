44f65cd6937045f4fdcf7f22d39cad4f808dcc8e
bala-amypo <bala@amypo.in>
2025-12-28 12:39:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferEvaluationResult.java
+++ src/main/java/com/example/demo/entity/TransferEvaluationResult.java
@@ -9,45 +9,17 @@ public class TransferEvaluationResult {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private Course sourceCourse;
-
-    @ManyToOne
-    private Course targetCourse;
-
-    private boolean eligible;
-
-    // ===== GETTERS & SETTERS =====
+    private double overlapPercentage;
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public double getOverlapPercentage() {
+        return overlapPercentage;
     }
 
-    public Course getSourceCourse() {
-        return sourceCourse;
-    }
-
-    public void setSourceCourse(Course sourceCourse) {
-        this.sourceCourse = sourceCourse;
-    }
-
-    public Course getTargetCourse() {
-        return targetCourse;
-    }
-
-    public void setTargetCourse(Course targetCourse) {
-        this.targetCourse = targetCourse;
-    }
-
-    public boolean isEligible() {
-        return eligible;
-    }
-
-    public void setEligible(boolean eligible) {
-        this.eligible = eligible;
+    public void setOverlapPercentage(double overlapPercentage) {
+        this.overlapPercentage = overlapPercentage;
     }
 }
