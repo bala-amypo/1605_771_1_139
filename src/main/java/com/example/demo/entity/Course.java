c1f1aa0437473b746da2e4fdb40243b5b9014a9d
bala-amypo <bala@amypo.in>
2025-12-28 11:37:17 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -1,24 +1,51 @@
 package com.example.demo.entity;
 
-
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Course {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String courseCode;
     private String courseName;
-    private int creditHours;
-    private boolean active = true;
 
-    @ManyToOne
-    @JoinColumn(name = "university_id")
-    private University university;
+    private boolean active;
+
+    private String university;
+
+    // ===== GETTERS & SETTERS =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
 }
