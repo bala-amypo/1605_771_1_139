dfe40f06a0ef005cb57b5b80be4a14ff8bf794fc
bala-amypo <bala@amypo.in>
2025-12-29 14:41:02 +0000

    User commit

--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -6,11 +6,14 @@ import java.util.List;
 public interface CourseService {
 
     Course createCourse(Course course);
-    Course updateCourse(Long id, Course course);
-    Course getCourseById(Long id);
-    List<Course> getAllCourses();
-    void deleteCourse(Long id);
 
-   
+    Course updateCourse(Long id, Course course);
+
+    Course getCourseById(Long id);
+
+    List<Course> getAllCourses();
+
     List<Course> getCoursesByUniversity(Long universityId);
+
+    void deleteCourse(Long id);
 }
