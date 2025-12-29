032ffa84400d2b6c93a4eef9e5d1f6a2d4657361
bala-amypo <bala@amypo.in>
2025-12-29 15:09:54 +0000

    User commit

--- src/main/java/com/example/demo/service/TransferRuleService.java
+++ src/main/java/com/example/demo/service/TransferRuleService.java
@@ -7,15 +7,7 @@ public interface TransferRuleService {
 
     TransferRule createRule(TransferRule rule);
 
-    TransferRule updateRule(Long id, TransferRule rule);
-
-    TransferRule getRuleById(Long id);
-
-    List<TransferRule> getAllRules();
-
-    void deleteRule(Long id);
-
-    TransferRule deactivateRule(Long id); 
     List<TransferRule> getRulesForUniversities(Long sourceId, Long targetId);
 
-} 
+    void deactivateRule(Long id);
+}
