4fe316280f65de3286d9100932448846defe4a59
bala-amypo <bala@amypo.in>
2025-12-29 15:10:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TransferRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TransferRuleServiceImpl.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.TransferRule;
 import com.example.demo.repository.TransferRuleRepository;
 import com.example.demo.service.TransferRuleService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +11,17 @@ import java.util.List;
 @Service
 public class TransferRuleServiceImpl implements TransferRuleService {
 
-    @Autowired
-    private TransferRuleRepository ruleRepository;
+    private final TransferRuleRepository ruleRepository;
+
+    public TransferRuleServiceImpl(TransferRuleRepository ruleRepository) {
+        this.ruleRepository = ruleRepository;
+    }
+
+    @Override
+    public TransferRule createRule(TransferRule rule) {
+        rule.setActive(true);
+        return ruleRepository.save(rule);
+    }
 
     @Override
     public List<TransferRule> getRulesForUniversities(Long sourceId, Long targetId) {
@@ -21,5 +29,13 @@ public class TransferRuleServiceImpl implements TransferRuleService {
                 .findBySourceUniversityIdAndTargetUniversityIdAndActiveTrue(
                         sourceId, targetId
                 );
+    }
+
+    @Override
+    public void deactivateRule(Long id) {
+        TransferRule rule = ruleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rule not found"));
+        rule.setActive(false);
+        ruleRepository.save(rule);
     }
 }
