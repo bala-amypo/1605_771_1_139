71051cc37dadeb11f1c0c39306614572d8441b93
bala-amypo <bala@amypo.in>
2025-12-29 14:10:48 +0000

    User commit

--- src/main/java/com/example/demo/repository/UniversityRepository.java
+++ src/main/java/com/example/demo/repository/UniversityRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.University;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UniversityRepository extends JpaRepository<University, Long> {
-   
+
+    Optional<University> findByName(String name);
 }
