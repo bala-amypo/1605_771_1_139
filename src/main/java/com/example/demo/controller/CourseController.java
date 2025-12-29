0fe9bb4a5b966b4f484902638d0c3ac642759aac
bala-amypo <bala@amypo.in>
2025-12-29 14:36:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,32 +1,27 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Course;
-import com.example.demo.service.CourseService;
+import com.example.demo.repository.CourseRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/courses")
+@RequestMapping("/courses")
 public class CourseController {
 
     @Autowired
-    private CourseService courseService;
+    private CourseRepository courseRepository;
 
     @PostMapping
-    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
-        return ResponseEntity.ok(courseService.createCourse(course));
+    public Course create(@RequestBody Course c) {
+        return courseRepository.save(c);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
-        return ResponseEntity.ok(courseService.getCourseById(id));
-    }
-
-    @GetMapping("/university/{universityId}")
-    public ResponseEntity<List<Course>> getCoursesByUniversity(@PathVariable Long universityId) {
-        return ResponseEntity.ok(courseService.getCoursesByUniversity(universityId));
+    @GetMapping
+    public List<Course> getAll() {
+        return courseRepository.findAll();
     }
 }
