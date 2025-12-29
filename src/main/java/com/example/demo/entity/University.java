444d16f2059d902359be89e909d510adaf9e6699
bala-amypo <bala@amypo.in>
2025-12-29 15:00:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -1,7 +1,6 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-
 @Entity
 public class University {
 
@@ -11,8 +10,22 @@ public class University {
 
     private String name;
 
+    private boolean active = true;
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     public String getName() {
