e7d6b6ddbfafcda3d367a437eb9efc208b14987c
bala-amypo <bala@amypo.in>
2025-12-29 15:23:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferRule.java
+++ src/main/java/com/example/demo/entity/TransferRule.java
@@ -2,5 +2,33 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "transfer_rules")
+public class TransferRule {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long sourceUniversityId;
+    private Long targetUniversityId;
+
+    private Long sourceCourseId;
+    private Long targetCourseId;
+
+    private Integer creditHourTolerance;
+    private Float minimumOverlapPercentage;
+
+    // 🔥 IMPORTANT: FIELD NAME MUST BE "active"
+    @Column(name = "is_active")
+    private boolean active;
+
+    // getters & setters
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
