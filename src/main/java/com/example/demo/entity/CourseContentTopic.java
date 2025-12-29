aa898e4410478379eb2920bdd642478d70bb0388
bala-amypo <bala@amypo.in>
2025-12-29 14:53:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/CourseContentTopic.java
+++ src/main/java/com/example/demo/entity/CourseContentTopic.java
@@ -3,57 +3,34 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "course_content_topics")
 public class CourseContentTopic {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    private String topicName;
+
     @ManyToOne
-    @JoinColumn(name = "course_id")
     private Course course;
-    
-    private String topic;
-    
-    @Column(name = "weight_percentage")
-    private Double weightPercentage;
-    
-    // Default constructor
-    public CourseContentTopic() {
-    }
-    
-    // Getters and Setters
+
     public Long getId() {
         return id;
     }
-    
-    public void setId(Long id) {
-        this.id = id;
+
+    public String getTopicName() {
+        return topicName;
     }
-    
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
     public Course getCourse() {
         return course;
     }
-    
+
     public void setCourse(Course course) {
         this.course = course;
-    }
-    
-   
-    public void setTopicName(String topicName) {
-    this.topicName = topicName;
-}
-
-public String getTopicName() {
-    return topicName;
-}
-
-    public Double getWeightPercentage() {
-        return weightPercentage;
-    }
-    
-    public void setWeightPercentage(Double weightPercentage) {
-        this.weightPercentage = weightPercentage;
     }
 }
